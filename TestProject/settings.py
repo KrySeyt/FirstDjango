8de86615dc50ba4a8b84bed217290ebe0ea111db
KrySeyt <kryseyt@gmail.com>
2022-03-06 13:28:05 +0300

    SSLOFF

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-xzg^9cx7qrgi!*3jr5%ecl%t@c(#1nyk9u3h2rfx5it!q=^w^y'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
